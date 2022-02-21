e50755bbea082dc2ed70fa24bb4ad021b4c5dbea
Joeic <97564910+Joeic@users.noreply.github.com>
2022-02-20 19:16:37 -0700

    Update SpreadsheetTests.cs

--- SpreadsheetTests/SpreadsheetTests.cs
+++ SpreadsheetTests/SpreadsheetTests.cs
@@ -5,7 +5,20 @@ using System.Collections.Generic;
 using SpreadsheetUtilities;
 using System.IO;
 
-
+/// <summary> 
+/// Author:    Joey Cai
+/// Partner:  None
+/// Date:      2/20/2022
+/// Course:    CS 3500, University of Utah, School of Computing 
+/// Copyright: CS 3500 and Joey Cai - This work may not be copied for use in Academic Coursework. 
+/// 
+/// I, Joey Cai, certify that I wrote this code from scratch and did not copy it in part or whole from  
+/// another source.  All references used in the completion of the assignment are cited in my README file. 
+/// 
+/// File Contents 
+/// 
+///  This proeject will test all methods from SpreadSheet
+/// </summary>
 namespace SpreadsheetTests
 {
 	[TestClass]
